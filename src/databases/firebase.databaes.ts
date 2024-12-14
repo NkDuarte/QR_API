35d6c8d41640d3e09127fe6d6565c199ea0b79df
NkDuarte <nkduarte22@gmail.com>
2024-12-14 14:10:54 -0500

    FEAT 🥳 |-> Implementacion MVP QR APP

--- src/databases/firebase.databaes.ts
+++ src/databases/firebase.databaes.ts
@@ -5,7 +5,7 @@ import path, { resolve } from 'path'
 import { _name_file_fire_conf_, _f_bucket_storage_, _f_real_database_ } from '../environments/environments'
 /*************************/
 //* |-> Ruta de archivo credencial
-const account_fire = resolve(path.join(__dirname, `../config/${_name_file_fire_conf_}`))
+const account_fire = resolve(path.join(__dirname, `${_name_file_fire_conf_}`))
 /*************************/
 //* |-> Inicializar Firebase Admin
 admin.initializeApp({
